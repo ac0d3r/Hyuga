26cf7d5d1e8d1c57d2583f5f5fb5246a8842972d
Buzz2d0 <admin@imipy.com>
2020-02-05 15:12:43 +0800

    feat: manage.py - add 'runweb' command

--- manage.py
+++ manage.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+from wsgiref import simple_server
+
 import click
 
 from hyuga.lib.option import init
@@ -12,14 +14,15 @@ BANNER = r"""
 """
 init()
 
+
 @click.group()
 def main():
     click.echo(BANNER)
 
 
 @main.command()
-@click.option('--username', 'username', prompt=True)
-@click.option('--password', 'password', prompt=True, hide_input=True,
+@click.option("--username", "username", prompt=True)
+@click.option("--password", "password", prompt=True, hide_input=True,
               confirmation_prompt=True)
 def createsuperuser(username, password):
     from hyuga.models.user import User
@@ -27,8 +30,8 @@ def createsuperuser(username, password):
     User.create(
         username=username,
         password=password,
-        identify='admin.hyuga.co',
-        token='',
+        identify="admin.hyuga.co",
+        token="",
         administrator=True
     )
     click.echo("[CMD] create super user success...")
@@ -42,6 +45,26 @@ def createtables():
     with database:
         database.create_tables([User], safe=True)
     click.echo("[CMD] create tables success...")
+
+
+@main.command()
+@click.option("--host", "host", default="127.0.0.1")
+@click.option("--port", "port", default=8080)
+def runweb(host, port):
+    """Runs the application on a local development server.
+    """
+    from hyuga.app import create
+    try:
+        app = create(testing=True)
+        click.echo("[TESTING] %s" % ("="*50))
+        click.echo("[TESTING] API Server run on: http://%s:%s" %
+                   (host, port))
+        click.echo("[TESTING] %s" % ("="*50))
+        httpd = simple_server.make_server(host, int(port), app)
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        click.echo("\nBye👋")
+        httpd.shutdown()
 
 
 @main.command()
