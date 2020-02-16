c96927147889ea76db9098c55e8b1083220cec59
Buzz2d0 <admin@imipy.com>
2020-02-16 21:31:38 +0800

    deploy: update '.gitignore' & 'bin/bin.sh'

--- bin/bin.sh
+++ bin/bin.sh
@@ -1,4 +1,4 @@
 echo "start api server use [gunicorn.conf]"
-gunicorn -c gunicorn.conf wsgi:app
+gunicorn -c gunicorn.conf hyuga.wsgi:app
 echo "start dns server"
-python manage.py rundns
+python -c "from hyuga.dns import dnsserver;dnsserver()"
