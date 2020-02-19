6dcb51b7a3999a5060d4aafc97b2d727bf3a34e0
Buzz2d0 <admin@imipy.com>
2020-02-19 22:19:55 +0800

    fix: allow all orgin only when huyga is debug

--- hyuga/middleware/common.py
+++ hyuga/middleware/common.py
@@ -4,14 +4,21 @@ from falcon.http_status import HTTPStatus
 
 from hyuga.core.database import database
 from hyuga.core.log import logger
+from hyuga.lib.option import LOG_LEVEL, CONFIG
 
 
 class HandleCORS:
     def process_request(self, req, resp):
-        resp.set_header('Access-Control-Allow-Origin', '*')
+        if LOG_LEVEL != "info":  # allow all orgin when huyga is debug
+            resp.set_header('Access-Control-Allow-Origin', '*')
+        else:
+            resp.set_header('Access-Control-Allow-Origin',
+                            "http://%s" % CONFIG.DOMAIN)
+
         resp.set_header('Access-Control-Allow-Methods', '*')
         resp.set_header('Access-Control-Allow-Headers', '*')
         resp.set_header('Access-Control-Max-Age', 24 * 3600)
+
         if req.method == 'OPTIONS':
             raise HTTPStatus(falcon.HTTP_200, body='\n')
 
