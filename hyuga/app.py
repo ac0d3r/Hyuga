d2cde49c4b59b582a17f8032f631bf94e34ec7a2
Buzz2d0 <admin@imipy.com>
2020-02-23 16:45:50 +0800

    feat: using falcon.App

--- hyuga/app.py
+++ hyuga/app.py
@@ -3,31 +3,29 @@ import falcon
 from hyuga.api.common import base
 from hyuga.api.v1 import records, users
 from hyuga.core.errors import AppError
-from hyuga.core.log import logger
 from hyuga.middleware import GlobalFilter, HandleCORS
 
 
-class App(falcon.API):
-    def __init__(self, *args, **kwargs):
-        super(App, self).__init__(*args, **kwargs)
-        logger.info('API Server is starting')
-        # index
-        self.add_route('/', base.BaseResource())
-        # users
-        self.add_route('/v1/users', users.Users())
-        self.add_route('/v1/users/{user_id:int}', users.UsersItem())
-        # users self
-        self.add_route('/v1/users/self', users.UsersSelf())
-        self.add_route('/v1/users/self/login', users.UsersSelfOperation())
-        # records
-        self.add_route('/v1/records', records.Records())
-        self.add_route('/v1/users/self/records', records.UsersSelfRecords())
+def init_routes(app):
+    # index
+    app.add_route('/', base.BaseResource())
+    # users
+    app.add_route('/v1/users', users.Users())
+    app.add_route('/v1/users/{user_id:int}', users.UsersItem())
+    # users self
+    app.add_route('/v1/users/self', users.UsersSelf())
+    app.add_route('/v1/users/self/login', users.UsersSelfOperation())
+    # records
+    app.add_route('/v1/records', records.Records())
+    app.add_route('/v1/users/self/records', records.UsersSelfRecords())
 
-        self.add_error_handler(AppError, AppError.handle)
+    app.add_error_handler(AppError, AppError.handle)
 
 
 def create_app(testing=False):
     middlewares = [HandleCORS()]
     if testing is False:
         middlewares.append(GlobalFilter())
-    return App(middleware=middlewares)
+    app = falcon.App(middleware=middlewares)
+    init_routes(app)
+    return app
