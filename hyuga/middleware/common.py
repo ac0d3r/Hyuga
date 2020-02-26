1926c260bf2cd22e42fa1bef635d6f0cde1b1698
Buzz2d0 <admin@imipy.com>
2020-02-26 10:18:01 +0800

    deploy: update requirements.txt

--- hyuga/middleware/common.py
+++ hyuga/middleware/common.py
@@ -8,12 +8,13 @@ from hyuga.lib.option import LOG_LEVEL, CONFIG
 
 class HandleCORS:
     def process_request(self, req, resp):
-        if LOG_LEVEL != "INFO":  # allow all orgin when huyga is debug
-            resp.set_header('Access-Control-Allow-Origin', '*')
-        else:
+        if LOG_LEVEL == "INFO":
             origin = req.get_header('ORIGIN', "")
             if origin and origin.startswith("http://%s" % CONFIG.DOMAIN):
                 resp.set_header('Access-Control-Allow-Origin', origin)
+        else:
+            # allow all orgin when huyga is debug
+            resp.set_header('Access-Control-Allow-Origin', '*')
         resp.set_header('Access-Control-Allow-Methods', '*')
         resp.set_header('Access-Control-Allow-Headers', '*')
         resp.set_header('Access-Control-Max-Age', 24 * 3600)
