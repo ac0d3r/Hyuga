c50e07afac0b361fb3cfd3e002ac5b7590ed75ab
Buzz2d0 <admin@imipy.com>
2020-02-26 09:39:36 +0800

    fix: handle CORS

--- hyuga/middleware/common.py
+++ hyuga/middleware/common.py
@@ -8,12 +8,12 @@ from hyuga.lib.option import LOG_LEVEL, CONFIG
 
 class HandleCORS:
     def process_request(self, req, resp):
-        if LOG_LEVEL != "info":  # allow all orgin when huyga is debug
+        if LOG_LEVEL != "INFO":  # allow all orgin when huyga is debug
             resp.set_header('Access-Control-Allow-Origin', '*')
         else:
-            resp.set_header('Access-Control-Allow-Origin',
-                            "http://%s" % CONFIG.DOMAIN)
-
+            origin = req.get_header('ORIGIN', "")
+            if origin and origin.startswith("http://%s" % CONFIG.DOMAIN):
+                resp.set_header('Access-Control-Allow-Origin', origin)
         resp.set_header('Access-Control-Allow-Methods', '*')
         resp.set_header('Access-Control-Allow-Headers', '*')
         resp.set_header('Access-Control-Max-Age', 24 * 3600)
