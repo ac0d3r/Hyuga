1b0b56d6a12f22f6a8a5b43628fc57f09321746c
whoami <admin@imipy.com>
2020-08-31 22:31:05 +0800

    deploy

---
+++ deploy/build.sh
@@ -0,0 +1,28 @@
+# build hyuga.go
+# CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" hyuga.go
+
+## CentOS install Nginx
+## https://segmentfault.com/a/1190000018109309
+sudo yum -y install epel-release
+sudo yum -y install nginx
+sudo systemctl enable nginx
+sudo systemctl start nginx
+sudo cp deploy/nginx-hyuga.conf /etc/nginx/conf.d/ # 配置文件
+sudo systemctl reload nginx
+## frontend files
+sudo mkdir -p /var/www/
+sudo cp frontend/* /var/www/
+
+## CentOS install Redis
+## https://zhuanlan.zhihu.com/p/34527270
+sudo yum -y install redis
+sudo systemctl enable redis.service
+sudo systemctl start redis
+
+## Supervisor
+sudo yum install -y supervisor
+sudo systemctl enable supervisord
+sudo systemctl start supervisord
+sudo cp deploy/supervisor-hyuga.conf /etc/supervisor/conf.d/ # 配置文件
+sudo systemctl reload supervisord
+sudo supervisorctl start hyuga
---
+++ deploy/deploy.md
@@ -0,0 +1,47 @@
+# Deploy
+## 修改环境变量
+- 修改 `.env` 文件或者写入环境变量
+
+```
+APP_ENV=[production/development]
+
+REDIS_SERVER=[localhost]
+REDIS_PORT=[6379]
+
+DOMAIN=[hyuga.io]
+NS1_DOMAIN=[ns1.app.io]
+NS2_DOMAIN=[ns2.app.io]
+SERVER_IP=[1.1.1.1]
+```
+- 修改 `nginx-hyuga.conf` 域名
+```
+server {
+    listen 80;
+    server_name [hyuga.io];
+...
+}
+server {
+    listen 80;
+    server_name *.[hyuga.io];
+....
+```
+
+## 需要的服务 services
+- redis
+- nginx
+- supervisor
+
+`deploy/build.sh` 只简简单编写 centos 系统的脚本
+
+### Nginx
+
+1. `vim /etc/nginx/nginx.conf` 注释掉主配置文件中的内容：
+```conf
+# listen       80 default_server;
+# listen       [::]:80 default_server;
+# server_name  _;
+# root         /usr/share/nginx/html;
+```
+
+2. 拷贝 `deploy/nginx-hyuga.conf` 配置文件到 `/etc/nginx/conf.d/` 目录下.
+3. 将 `frontend/*` 下的前端文件拷贝到 `nginx` 配置文件 `root` 目录下。
